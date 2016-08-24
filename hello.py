6ef7b0e79467b5a58ab4ffbf55b83064e6778e24
1dot75cm <sensor.wen@gmail.com>
2016-08-25 00:39:28 +0800

    Asynchronous emails
    异步发送邮件

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import os
+from threading import Thread
 from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
@@ -66,13 +67,22 @@ class NameForm(Form):
     submit = SubmitField('Submit')  # 提交按钮
 
 
+def send_async_email(app, msg):
+    '''异步发送邮件'''
+    # SMTPAuthenticationError
+    # https://github.com/miguelgrinberg/flasky/issues/85
+    with app.app_context():
+        mail.send(msg)
+
 def send_email(to, subject, template, **kwargs):
     '''发送邮件'''
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 def make_shell_context():
