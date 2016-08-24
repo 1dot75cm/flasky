9d242536e26a33427f0de32df92e7056e5dc16c6
1dot75cm <sensor.wen@gmail.com>
2016-08-24 20:21:40 +0800

    Database models with Flask-SQLAlchemy
    Flask-SQLAlchemy 扩展包装了 SQLAlchemy 框架, 用于处理高度抽象的数据库对象

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -6,13 +7,44 @@ from flask_moment import Moment
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)  # 实例化app
 app.config['SECRET_KEY'] = 'secret_key_string'  # 用于加密 session 的密钥
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')  # 数据库URI
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  # 请求结束后, 自动提交
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    '''roles表模型'''
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')  # 返回与角色关联的用户列表
+    # backref 向 User 模型添加 role 属性, 从而定义反向关系
+    # role 属性可代替 role_id 访问 Role 模型, 获取模型对象
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    '''users表模型'''
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))  # 外键, 值为 roles.id
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(Form):
@@ -31,6 +63,7 @@ def internal_server_error(e):
     '''自定义500页面'''
     return render_template('500.html'), 500
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     '''主页视图函数'''
@@ -43,5 +76,7 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
 
+
 if __name__ == '__main__':
+    db.create_all()  # 创建所有表
     manager.run()
