39c2ddccea8799b9ec27ad721f6ee23251cee5eb
1dot75cm <sensor.wen@gmail.com>
2016-08-24 18:12:17 +0800

    Templates
    使用 render_template 渲染模板

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import Flask
+from flask import Flask, render_template
 from flask_script import Manager
 
 app = Flask(__name__)  # 实例化app
@@ -8,12 +8,12 @@ manager = Manager(app)
 @app.route('/')
 def index():
     '''主页视图函数'''
-    return '<h1>Hello, Flask!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
     '''User视图函数'''
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     manager.run()
