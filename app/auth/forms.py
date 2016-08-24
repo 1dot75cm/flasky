eb7681c0cfdd81f5e1cea320ee141087fc6d8384
1dot75cm <sensor.wen@gmail.com>
2016-08-25 07:11:27 +0800

    Change password
    修改密码

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -35,3 +35,12 @@ class RegistrationForm(Form):
         '''附加验证; 验证username无重复'''
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
+
+
+class ChangePasswordForm(Form):
+    '''修改密码表单'''
+    old_password = PasswordField('Old password', validators=[Required()])
+    password = PasswordField('New password', validators=[
+        Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm new password', validators=[Required()])
+    submit = SubmitField('Update Password')
