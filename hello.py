f843f741cc746c9bb4095fa2769e238f37847a49
1dot75cm <sensor.wen@gmail.com>
2016-08-24 17:59:12 +0800

    Command line options with Flask-Script
    Flask-Script 扩展为 Flask app 添加命令行选项

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 from flask import Flask
+from flask_script import Manager
+
 app = Flask(__name__)  # 实例化app
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -13,4 +16,4 @@ def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
