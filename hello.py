61f3812f7a7e1aafb6a569e980c840e906e7e1f6
1dot75cm <sensor.wen@gmail.com>
2016-08-24 19:17:27 +0800

    Dates and times with Flask-Moment
    Flask-Moment 扩展将 moment.js 集成至 Jinja2 模板

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
 # coding: utf-8
+from datetime import datetime
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)  # 实例化app
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -20,7 +23,8 @@ def internal_server_error(e):
 @app.route('/')
 def index():
     '''主页视图函数'''
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
