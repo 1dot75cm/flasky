1451a490a047deec0a6c3001947dec9264d8e511
1dot75cm <sensor.wen@gmail.com>
2016-09-05 06:20:00 +0800

    Fixes time format and templates

--- app/static/posts.js
+++ app/static/posts.js
@@ -1,6 +1,6 @@
 // 为相对时间添加 title 属性
 $(document).ready(function() {
-    $(".post-date, .comment-date span").each(function() {
+    $(".post-date span, .comment-date span").each(function() {
         var ts = $(this).attr('data-timestamp');
         var t = moment(ts).format('LLL');
         $(this).attr('title', t);
